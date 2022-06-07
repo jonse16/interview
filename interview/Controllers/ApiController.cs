5fcf96ce7633675ac7058c2787ca9ba468607aea
哲麒 林 <jonse2425@gmail.com>
2022-06-07 15:00:22 +0800

    新增邏輯 畫面ui建立DB物件 與方法

---
+++ interview/Controllers/ApiController.cs
@@ -0,0 +1,114 @@
+﻿using interview.Services;
+using interview.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace interview.Controllers
+{
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        MessageRepository messageRepository = new MessageRepository();
+
+        [HttpGet("list/house")]
+        public ContentResult listHouse() {
+            JObject result = new JObject();
+            try
+            {
+                JArray array = new JArray();
+                List<House> hList = messageRepository.listHouse();
+                foreach (House item in hList) {
+                    JObject obj = new JObject();
+                    obj.Add("id", item.id);
+                    obj.Add("name", item.name);
+                    array.Add(obj);
+                }
+                result.Add("status", 200);
+                result.Add("data", array);
+                return Content(result.ToString(), "application/json; charset=utf-8");
+            }
+            catch (Exception ex) {
+                result.Add("status", 500);
+                return Content(result.ToString(), "application/json; charset=utf-8");
+            }
+         
+        }
+        [HttpGet("list/msg")]
+        public ContentResult listMessage(int houseId)
+        {
+            JObject result = new JObject();
+            try
+            {
+                JArray array = new JArray();
+                List<Message> mList =  messageRepository.listMessage(houseId);
+                foreach (Message item in mList) {
+                    JObject obj = new JObject();
+                    obj.Add("id", item.id);
+                    obj.Add("name", item.name);
+                    obj.Add("msg", item.msg);
+                    obj.Add("lastUpdateDateTime", item.lastUpdateDateTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                    array.Add(obj);
+
+                }
+                result.Add("status", 200);
+                result.Add("data", array);
+                return Content(result.ToString(), "application/json; charset=utf-8");
+            }
+            catch (Exception ex)
+            {
+                result.Add("status", 500);
+                return Content(result.ToString(), "application/json; charset=utf-8");
+            }
+        }
+
+        [HttpPost("add")]
+        public ContentResult addMessage(int houseId, string name, string msg)
+        {
+            JObject result = new JObject();
+            try
+            {
+                messageRepository.addMessage(houseId, name, msg);
+                result.Add("status", 200);
+                return Content(result.ToString(), "application/json; charset=utf-8");
+            }
+            catch (Exception ex)
+            {
+                result.Add("status", 500);
+                return Content(result.ToString(), "application/json; charset=utf-8");
+            }
+        }
+        [HttpPost("edit")]
+        public ContentResult editMessage(int msgId, string msg)
+        {
+            JObject result = new JObject();
+            try
+            {
+                messageRepository.editMessage(msgId, msg);
+                result.Add("status", 200);
+                return Content(result.ToString(), "application/json; charset=utf-8");
+            }
+            catch (Exception ex)
+            {
+                result.Add("status", 500);
+                return Content(result.ToString(), "application/json; charset=utf-8");
+            }
+        }
+        [HttpDelete("delete")]
+        public ContentResult deleteMessage(int msgId)
+        {
+            JObject result = new JObject();
+            try
+            {
+                messageRepository.deleteMessage(msgId);
+                result.Add("status", 200);
+                return Content(result.ToString(), "application/json; charset=utf-8");
+            }
+            catch (Exception ex)
+            {
+                result.Add("status", 500);
+                return Content(result.ToString(), "application/json; charset=utf-8");
+            }
+        }
+
+    }
+}
---
+++ interview/Services/MessageRepository.cs
@@ -0,0 +1,96 @@
+﻿using interview.Tool;
+using interview.Models;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Text;
+namespace interview.Services
+{
+    public class MessageRepository
+    {
+        MySqlConnection conn = new MySqlConnection("server=127.0.0.1;port=3306;user id=root;password=root;database=interview;charset=utf8;SslMode=none;Allow User Variables=True");
+        ModelsConverter mc = new ModelsConverter();
+
+        public string nowString() {
+
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+        public List<House> listHouse()
+        {
+            string sql = "SELECT * FROM house";
+            DataTable dt = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+            adapter.Fill(dt);
+            if (dt.Rows.Count == 0)
+            {
+                return new List<House>();
+            }
+            else
+            {
+                return mc.convetToHouse(dt);
+            }
+        }
+
+        public List<Message> listMessage(int houseId)
+        {
+            string sql = "SELECT * FROM message where house_id = {0} and deleted = 0";
+            List<Object> param = new List<object>();
+            param.Add(houseId);
+            sql = string.Format(sql, param.ToArray());
+            DataTable dt = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+            adapter.Fill(dt);
+            if (dt.Rows.Count == 0)
+            {
+                return new List<Message>();
+            }
+            else
+            {
+                return mc.convetToMessage(dt);
+            }
+        }
+
+        public void addMessage(int houseId, string name, string msg)
+        {
+            string now = nowString();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("INSERT INTO message(name, msg, house_id, create_date_time, last_update_date_time) ");
+            sb.Append("VALUES({0}, {1}, {2}, {3}, {3})");
+            List<Object> param = new List<object>();         
+            param.Add(name);
+            param.Add(msg);
+            param.Add(houseId);
+            param.Add(now);
+            string sql = string.Format(sb.ToString(), param.ToArray());
+            DataTable dt = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+            adapter.Fill(dt);
+        }
+
+        public void editMessage(int msgId, string msg)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("UPDATE  message set msg = {1}, last_update_date_time = {2}  WHERE id = {0}");
+            List<Object> param = new List<object>();
+            param.Add(msgId);
+            param.Add(msg);
+            param.Add(nowString());
+            string sql = string.Format(sb.ToString(), param.ToArray());
+            DataTable dt = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+            adapter.Fill(dt);
+        }
+
+        public void deleteMessage(int msgId)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("DELETE FROM message WHERE id = {0}");
+            List<Object> param = new List<object>();
+            param.Add(msgId);
+            string sql = string.Format(sb.ToString(), param.ToArray());
+            DataTable dt = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+            adapter.Fill(dt);
+        }
+    }
+}
